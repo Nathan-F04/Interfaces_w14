85799941de3e063a8aa74d90df26734a86801b9d
G00413711@atu.ie <G00413711@atu.ie>
2024-02-08 12:16:49 +0000

    Added Dessert class and tested it.

--- src/main/java/ie/atu/Dessert.java
+++ src/main/java/ie/atu/Dessert.java
@@ -1,17 +1,17 @@
 package ie.atu;
 
-public class Burger implements MenuItem{
+public class Dessert implements MenuItem {
     private String name;
     private double price;
     private String ingredients;
 
-    public Burger(String name,double price,String ingredients){
+    public Dessert(String name,double price,String ingredients){
         this.name=name;
         this.price=price;
         this.ingredients=ingredients;
     }
 
-    public Burger() {
+    public Dessert() {
         this.name = " ";
         this.ingredients = " ";
         this.price = 0;
@@ -31,5 +31,6 @@ public class Burger implements MenuItem{
     public String getDescription(){
         return ingredients;
     }
+
 
 }
